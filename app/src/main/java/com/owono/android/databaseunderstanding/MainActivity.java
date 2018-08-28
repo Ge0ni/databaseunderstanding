b1c9a5de1c17b60d3e8b3a8aa3a993487f123303
Ge0ni <gogo_aog@yahoo.fr>
2018-08-29 00:44:53 +0200

    2nd commit. working on the delete function

--- app/src/main/java/com/owono/android/databaseunderstanding/MainActivity.java
+++ app/src/main/java/com/owono/android/databaseunderstanding/MainActivity.java
@@ -1,13 +1,96 @@
 package com.owono.android.databaseunderstanding;
 
+import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseHelper myDb;
+
+    EditText editName, editSurname, editMarks, editId;
+    Button btnAddData, btnViewAll, btnUpdate, btnDelete;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        myDb = new DatabaseHelper(this);
+
+        editName = (EditText) findViewById(R.id.editText_Name);
+        editSurname = (EditText) findViewById(R.id.editText_Surname);
+        editMarks = (EditText) findViewById(R.id.editText_Marks);
+        editId = (EditText) findViewById(R.id.editText_id);
+        btnAddData = (Button) findViewById(R.id.button_add);
+        btnViewAll = (Button) findViewById(R.id.button_viewAll);
+        btnUpdate = (Button) findViewById(R.id.button_update);
+        btnDelete = (Button) findViewById(R.id.button_delete);
+
+        addData();
+        viewAll();
+        updateData();
+    }
+
+    public void addData(){
+        btnAddData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isInserted = myDb.insertData(editName.getText().toString(), editSurname.getText().toString(), editMarks.getText().toString());
+                if (isInserted)
+                    Toast.makeText(MainActivity.this, "Data inserted", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not inserted", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    public void viewAll(){
+        btnViewAll.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Cursor res = myDb.getAllData();
+                if (res.getCount() == 0) {
+                    // show message that there is no data
+                    showMessage("Error", "No data found");
+                    return;
+                }
+                StringBuffer buffer = new StringBuffer();
+                while(res.moveToNext()){
+                    buffer.append("Id: " + res.getString(0) + "\n");
+                    buffer.append("Name: " + res.getString(1) + "\n");
+                    buffer.append("Surname: " + res.getString(2) + "\n");
+                    buffer.append("Marks: " + res.getString(3) + "\n\n");
+                }
+
+                // show All data
+                showMessage("Data", buffer.toString());
+            }
+        });
+    }
+
+    public void showMessage(String title, String message){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
+    }
+
+    public void updateData() {
+        btnUpdate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                boolean isUpdate = myDb.updateData(editId.getText().toString(), editName.getText().toString(), editSurname.getText().toString(), editMarks.getText().toString());
+                if (isUpdate)
+                    Toast.makeText(MainActivity.this, "Data updated", Toast.LENGTH_LONG).show();
+                else
+                    Toast.makeText(MainActivity.this, "Data not updated", Toast.LENGTH_LONG).show();
+            }
+        });
     }
 }
